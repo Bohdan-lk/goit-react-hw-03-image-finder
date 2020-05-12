a6df7a2d47ef1d3bcb1b5129f141f824afc689b5
Bohdan-lk <50949795+Bohdan-lk@users.noreply.github.com>
2020-05-12 19:47:27 +0300

    create app

--- src/services/Api.js
+++ src/services/Api.js
@@ -7,7 +7,9 @@ const fetchImagesWithQuery = (searchQuery, page = 1) => {
       `https://pixabay.com/api/?q=${searchQuery}&page=${page}&key=${key}&image_type=photo&orientation=horizontal&per_page=12`
     )
     .then((response) => response.data.hits);
+
 };
+
 
 export default {
   fetchImagesWithQuery,
